b94d7b0d327b9ecbafaa0ae604b592ae6474743d
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2025-02-10 13:12:03 +0700

    Исправил ШАХМАТЫ

--- chess.py
+++ chess.py
@@ -10,6 +10,8 @@ fig, ax = plt.subplots()
 s = ["A","B","C","D","E","F","G","H"]
 x = str(input("Введите заглавную букву:"))
 y = int(input("Введите цифру:"))
+# x = 5
+# y = 5
 y = 8-y
 for i in range(0,8):
     if x == s[i]:
@@ -24,40 +26,36 @@ plt.xticks(range(8), labels=s)
 plt.yticks(range(8), labels=[f"{i}" for i in range(8, 0, -1)])
 plt.title("Шахматная доска")
 
-for i in range(0,8,1):
-
+for i in range(0,8,1):#горизонталь
     circle_1 = plt.Circle((i, y), 0.1, color="green")
     ax.add_patch(circle_1)
-
-for i in range(0,8,1):
-
+for i in range(0,8,1):#вертикаль
     circle_1 = plt.Circle((x, i), 0.1, color="green")
     ax.add_patch(circle_1)
-
-for i in range(0,8,1):
-    if i+x>7 or i+y>7:
-        break
-
-    circle_1 = plt.Circle((x+i,y+i), 0.1, color="green")
-    ax.add_patch(circle_1)
-for i in range(0,7,1):
-    if x-i< 0 or y+i > 7:
+for i in range(0,7,1):#лево верх
+    if x-i< 0 or y-i <0:
         break
     circle_1 = plt.Circle((x-i,y-i), 0.1, color="green")
     ax.add_patch(circle_1)
-for i in range(0,8,1):
-    if i+ x > 7 or -(i - y) > 7:
+for i in range(0,8,1):#право верх
+    if i+ x > 7 or -(i - y) < 0:
         break
     circle_1 = plt.Circle((x+i,y -i), 0.1, color="green")
     ax.add_patch(circle_1)
-for i in range(0,8,1):
-    if -(i- x) > 7 or i + y > 7:
+for i in range(0,8,1):#лево низ
+    if -(i- x) <0 or i + y > 7:
         break
     circle_1 = plt.Circle((x-i,y +i), 0.1, color="green")
+    ax.add_patch(circle_1)
+for i in range(0,8,1):#лево низ
+    if x+i > 7 or i + y > 7:
+        break
+    circle_1 = plt.Circle((x+i,y +i), 0.1, color="green")
     ax.add_patch(circle_1)
 #создали ферзя
 circle = plt.Circle((x, y), 0.4, color= "red", label="ФЕРЗЬ")
 ax.add_patch(circle)
-
+plt.xlim(-5,10)
+plt.ylim(15,-5)
 plt.legend(loc=(1, 1))
 plt.show()
